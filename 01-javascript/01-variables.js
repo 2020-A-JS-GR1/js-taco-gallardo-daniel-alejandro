562bff8658c51b15d2a5d7046844e4b2c3995377
Daniel Taco <daniel.taco@epn.edu.ec>
2020-06-12 17:20:39 -0500

    variables y console typeof

---
+++ 01-javascript/01-variables.js
@@ -0,0 +1,27 @@
+//Mutables e Inmutables
+
+//Variables Mutables
+var numeroUno = 1;
+let numeroDos = 2;
+numeroUno = 5;
+numeroDos = 8;
+numeroUno = false;
+numeroDos = true;
+
+//Variables Inmutables
+const configuracionArchivos = 'PDF';
+//configuracionArchivos = 'XML'
+
+// Tipos de variables
+const numero = 1; //number
+const sueldo = 1.2; //number
+const texto = 'Adrian'; //string
+const booleano = false; //boolean
+const hijos = null; //object
+const zapatos = undefined; //undefined
+console.log(typeof numero);
+console.log(typeof sueldo);
+console.log(typeof texto);
+console.log(typeof booleano);
+console.log(typeof hijos);
+console.log(typeof zapatos);
