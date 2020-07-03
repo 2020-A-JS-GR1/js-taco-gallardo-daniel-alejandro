661df63d9271f953bda846ebea01a3a2acafb733
Daniel Taco <daniel.taco@epn.edu.ec>
2020-07-03 14:41:31 -0500

    promesas

---
+++ 01-javascript/08-promesas.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+
+function promesaEsPar(numero) {
+    const miPrimerPromesa = new Promise( // Definicion de la promesa
+        (resolve, reject) => {
+            if (numero % 2 == 0) {
+                resolve(numero);
+            } else {
+                reject('No es par =(');
+            }
+        }
+    )
+    return miPrimerPromesa
+}
+
+
+function promesaElevarAlCuadrado(numero) {
+    const miPrimerPromesa = new Promise( // Definicion de la promesa
+        (resolve, reject) => {
+            const numeroElevadoAlCuadrado = Math.pow(numero, 2);
+            resolve(numeroElevadoAlCuadrado);
+        }
+    );
+    return miPrimerPromesa
+}
+promesaEsPar(6)
+    .then( // try
+        (numeroPar) => {
+            console.log('Contenido then', numeroPar);
+            /*
+            NO HACER ESTO
+            promesaEsPar(2)
+                .then( // try
+                    (contenido) => {
+                        console.log('Contenido then', contenido);
+                    }
+                )
+                .catch( // catch
+                    (error) => {
+                        console.error('Contenido catch', error);
+                    }
+                )
+            */
+            return promesaElevarAlCuadrado(numeroPar) // promesa !
+        }
+    )
+    .then(
+        (numeroParAlCuadrado) => {
+            console.log('Numero par al cuadrado: ', numeroParAlCuadrado)
+        }
+    )
+    .catch( // catch
+        (error) => {
+            console.error('Contenido catch', error);
+        }
+    )
